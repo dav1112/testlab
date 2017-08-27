5e593eb154bf516039996b1f1a13ad097b49fc56
dav1112 <784648999@qq.com>
2017-08-27 16:16:13 +0800

    111

--- src/testlab/TestBrace.java
+++ src/testlab/TestBrace.java
@@ -5,7 +5,7 @@ public class TestBrace {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String str = " kk  oo uuui-i - iuu   dd ";
-		str = str.trim().replaceAll("(\\ )+", "-").replaceAll("(\\-)+", "-");
+		str = str.trim().replaceAll("(\\  )+", "-").replaceAll("(\\-)+", "-");
 		System.out.println(str);
 	}
 
